0621d94b9d8da14d8f8615d89eb90c44136be395
ashn <me@ashn.dev>
2024-03-12 08:43:37 -0400

    Use intmax_t for tv_nsec value

--- util.c
+++ util.c
@@ -3,11 +3,13 @@
 #include <stdio.h>
 #include <time.h>
 
+#define NSEC_PER_SEC 1000000000
+
 void
 sleep_sec(double sec)
 {
     time_t s = (time_t)sec;
-    double n = ((double)s - sec) * 1000000000;
+    intmax_t n = (s - (intmax_t)sec) * NSEC_PER_SEC;
     struct timespec ts = {
         .tv_sec = s,
         .tv_nsec = n,
